fe0ed7dc5e49e9b0b35090e255fdf7a7069b5266
Ashlin Furtado <92781171+ashlin07@users.noreply.github.com>
2024-03-09 14:45:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"I have changed the file,made a commit";
+  cout<<"I have changed the file,made a new commit";
   return 0;
 }
